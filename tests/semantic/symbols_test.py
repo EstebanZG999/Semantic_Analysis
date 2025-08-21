8240b40991bc73bf93b102d85ad7761650ff1f04
EstebanZG999 <zam22119@uvg.edu.gt>
2025-08-20 23:40:01 -0600

    test: testing different symbols

---
+++ tests/semantic/symbols_test.py
@@ -0,0 +1,18 @@
+from program.semantic.symbols import VarSymbol, ParamSymbol, FuncSymbol, ClassSymbol
+from program.semantic.typesys import INTEGER, STRING, make_fn
+
+def test_varsymbol():
+    x = VarSymbol(name="x", type=INTEGER, is_const=False, is_initialized=True)
+    assert x.name == "x" and x.type == INTEGER and not x.is_const and x.is_initialized
+
+def test_funcsymbol():
+    ftype = make_fn([INTEGER, STRING], INTEGER)
+    p0 = ParamSymbol(name="a", type=INTEGER, index=0)
+    p1 = ParamSymbol(name="b", type=STRING, index=1)
+    f = FuncSymbol(name="foo", type=ftype, params=(p0, p1))
+    assert f.type.ret == INTEGER and len(f.params) == 2
+
+def test_classsymbol():
+    c = ClassSymbol(name="Point", type=STRING) 
+    c.fields["x"] = VarSymbol(name="x", type=INTEGER)
+    assert "x" in c.fields
