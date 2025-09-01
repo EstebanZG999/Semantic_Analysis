14af1f6f7280fe954a6e4e0ce81dd7a2302d43a3
EstebanZG999 <zam22119@uvg.edu.gt>
2025-08-31 20:11:36 -0600

    feat: test infrastructure

---
+++ tests/semantic/util.py
@@ -0,0 +1,21 @@
+from antlr4 import InputStream, CommonTokenStream
+from program.CompiscriptLexer import CompiscriptLexer
+from program.CompiscriptParser import CompiscriptParser
+from program.semantic.type_checker import TypeChecker
+from program.semantic.error_reporter import ErrorReporter
+from program.semantic.scopes import GlobalScope
+
+def compile_source(source: str):
+    """
+    Compila una cadena de código Compiscript y devuelve (reporter, checker).
+    """
+    input_stream = InputStream(source)
+    lexer = CompiscriptLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = CompiscriptParser(stream)
+    tree = parser.program()
+
+    reporter = ErrorReporter()
+    checker = TypeChecker(reporter, GlobalScope())
+    checker.visit(tree)
+    return reporter, checker
